35f0de66637017c2dfa08ebd12b21c2eb2937166
adrien balleyguier <adriendino.balleyguier@master.hes-so.ch>
2025-05-27 03:18:06 +0200

    feat: compute block on the way. Some mistakes still remain but will wait some hours prior to getting fixed

--- scalp_board_files/scalp_revc_windows/scalp_user_design/scalp_user_design.srcs/sources_1/new/compute.vhd
+++ scalp_board_files/scalp_revc_windows/scalp_user_design/scalp_user_design.srcs/sources_1/new/compute.vhd
@@ -24,38 +24,72 @@ use IEEE.std_logic_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
--- TODO : add counter
-
 entity compute is
-    Port ( zn_re : in std_logic_vector (15 downto 0) := (others => '0');
-           zn_im : in std_logic_vector (15 downto 0) := (others => '0');
-           c_re : in std_logic_vector (15 downto 0) := (others => '0');
-           c_im : in std_logic_vector (15 downto 0) := (others => '0');
-           znp1_re : out std_logic_vector (15 downto 0) := (others => '0');
-           znp1_im : out std_logic_vector (15 downto 0) := (others => '0');
-           lux : out std_logic := '0';
-           done : out std_logic := '0';
-           x : inout std_logic_vector (9 downto 0) := (others => '0');
-           y : inout std_logic_vector (9 downto 0) := (others => '0')
+    Port ( 
+        nrst, clk : in std_logic;
+        saved, done : inout std_logic;
+        lux: out std_logic;
+        c_re, c_im, z_n_re, z_n_im : in std_logic_vector(15 downto 0); -- 3 bits decimal
+        x, y : inout std_logic_vector(9 downto 0);
+        watcher : inout std_logic_vector(31 downto 0);
+        watcher_2 : out std_logic_vector(15 downto 0);
+        z_np1_re : out std_logic_vector(15 downto 0);
+        z_np1_im : out std_logic_vector(15 downto 0) -- 3 bits decimal
     );
 end compute;
 
-architecture behave_compute of compute is
-    signal zn_re_sq: std_logic_vector (31 downto 0) := (others => '0');
-    signal zn_im_sq: std_logic_vector (31 downto 0) := (others => '0');
-    signal zn_re_im: std_logic_vector (31 downto 0) := (others => '0');
-    signal zn_re_im_slice: std_logic_vector (17 downto 2) := (others => '0');
-    signal zn_re_im_sub: std_logic_vector (31 downto 0) := (others => '0');
-    signal zn_re_im_sub_slice: std_logic_vector (18 downto 3) := (others => '0');
-    signal norm: std_logic_vector (31 downto 0) := (others => '0');
-    signal norm_slice: std_logic_vector (18 downto 2) := (others => '0');
+architecture Behavioral of compute is
+    signal z_n_re_im : std_logic_vector(31 downto 0); -- 6 bits decimal
+    signal z_n_re_im_double : std_logic_vector(15 downto 0); -- 3 bits decimal, includes x2
+    signal z_n_re_sqrd : std_logic_vector(31 downto 0); -- 6 bits decimal
+    signal z_n_im_sqrd : std_logic_vector(31 downto 0); -- 6 bits decimal
+    signal norm : std_logic_vector(5 downto 0); 
+    signal z_n_sqrd_sub : std_logic_vector(31 downto 0);
+    signal z_n_sqrd_sub_slice : std_logic_vector(15 downto 0); -- 3 bits decimal
+    signal cntr : std_logic_vector(8 downto 0);
+    signal z_np1_im_delay : std_logic_vector(15 downto 0);
+    constant NORM_DIVERGE : integer range 0 to 7 := 4;
+    constant CNTR_LIMIT : integer range 0 to 511 := 300;
 begin
-    
+    process(clk, nrst)
+    begin
+        if (nrst = '0') then
+            cntr <= (others => '0');
+        elsif rising_edge(clk) then
+            if not (done = '1') then
+                z_n_re_im <= std_logic_vector(signed(z_n_re) * signed(z_n_im));
+                z_n_re_im_double <= z_n_re_im(27 downto 12);
+                watcher <= std_logic_vector(signed(z_n_re) * signed(z_n_im));
+                watcher_2<= z_n_re_im_double;
+                z_n_re_sqrd <= std_logic_vector(signed(z_n_re) * signed(z_n_re));
+                z_n_im_sqrd <= std_logic_vector(signed(z_n_im) * signed(z_n_im));
+                norm <= std_logic_vector(unsigned(z_n_re_sqrd) + unsigned(z_n_im_sqrd));
+                z_n_sqrd_sub <= std_logic_vector(unsigned(z_n_re_sqrd) - unsigned(z_n_im_sqrd));
+                z_n_sqrd_sub_slice <= z_n_sqrd_sub(31 downto 16);
+                cntr <= std_logic_vector(unsigned(cntr) + "1");
+                z_np1_im_delay <= std_logic_vector(signed(z_n_re_im_double) + signed(c_im));
+                -- out
+                if (unsigned(norm) >= NORM_DIVERGE) then
+                    lux <= '0';
+                    done <= '1';
+                else
+                    lux <= '1';
+                    if (unsigned(cntr) >= CNTR_LIMIT) then
+                        done <= '1';
+                    else
+                        done <= '0';
+                    end if;
+                end if;
+                z_np1_im <= z_np1_im_delay;
+                z_np1_re <= std_logic_vector(signed(z_n_sqrd_sub_slice) + signed(c_re));
+            end if;
+        end if;
+    end process;
 end Behavioral;
---
+++ scalp_board_files/scalp_revc_windows/scalp_user_design/scalp_user_design.srcs/sources_1/new/testbench_compute.vhd
@@ -0,0 +1,114 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 05/26/2025 10:43:26 AM
+-- Design Name: 
+-- Module Name: testbench_compute - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity testbench_compute is
+    generic(
+        C_RE : std_logic_vector(15 downto 0) := "1111110000010000"; -- -0.123
+        C_IM : std_logic_vector(15 downto 0) := "0001011111011111"; -- +0.745
+        NB_ITER_MAX : integer range 0 to 127 := 127; -- computation goes to 100, going a bit more to check behaviour
+        -- expecting test_A to diverge directly
+        TEST_A_RE : std_logic_vector(15 downto 0) := "0010000000000000"; -- -2
+        TEST_A_IM : std_logic_vector(15 downto 0) := "1110000000000000"; -- +2
+        -- expecting test_B to converge
+        TEST_B_RE : std_logic_vector(15 downto 0) := "0000000000000000"; -- 0
+        TEST_B_IM : std_logic_vector(15 downto 0) := "0000000000000000"; -- 0
+        -- expecting test_C to diverge around 50 iterations
+        TEST_C_RE : std_logic_vector(15 downto 0) := "0000110000011101"; -- 0.3785400390625
+        TEST_C_IM : std_logic_vector(15 downto 0) := "1111100110011111"; -- -0.1993408203125
+        -- expecting test_D to diverge around 95 iterations
+        TEST_D_RE : std_logic_vector(15 downto 0) := "1111100001110010"; -- -0.236083984375
+        TEST_D_IM : std_logic_vector(15 downto 0) := "1111011110010011" -- -0.2633056640625
+    );
+end testbench_compute;
+
+architecture Behavioral of testbench_compute is
+    component compute is 
+        port(
+            nrst, clk : in std_logic;
+            saved, done : inout std_logic;
+            lux: out std_logic;
+            c_re, c_im, z_n_re, z_n_im : in std_logic_vector(15 downto 0);
+            x, y : inout std_logic_vector(9 downto 0);
+            watcher : inout std_logic_vector(31 downto 0);
+            watcher_2 : out std_logic_vector(15 downto 0);
+            z_np1_re, z_np1_im : out std_logic_vector(15 downto 0)
+        );
+    end component;
+    signal nrst, clk, saved, lux, done : std_logic;
+    signal z_n_re, z_n_im, z_np1_re, z_np1_im : std_logic_vector(15 downto 0);
+    signal watcher : std_logic_vector(31 downto 0);
+    signal watcher_2 : std_logic_vector(15 downto 0);
+    signal x, y : std_logic_vector (9 downto 0);
+    constant CLK_PERIOD : time := 1 ns;
+begin
+    comp : compute
+    port map(
+        nrst => nrst, clk => clk, saved => saved, lux => lux, done => done,
+        c_re => C_RE, c_im => C_IM, z_n_re => z_n_re, z_n_im => z_n_im,
+        x => x, y => y,
+        watcher => watcher,
+        watcher_2 => watcher_2,
+        z_np1_re => z_np1_re, z_np1_im => z_np1_im
+    );
+
+    clk_process : process
+    begin
+        clk <= '0';
+        wait for CLK_PERIOD/2;
+        clk <= '1';
+        wait for CLK_PERIOD/2;
+    end process clk_process;
+
+    rst_process : process
+    begin
+        nrst <= '0';
+        wait for CLK_PERIOD;
+        nrst <= '1';
+        wait; -- forever
+    end process rst_process;
+
+    test_process : process
+    begin
+        wait for CLK_PERIOD;
+        z_n_re <= TEST_A_RE;
+        z_n_im <= TEST_A_IM;
+        for i in 0 to NB_ITER_MAX loop
+            wait for CLK_PERIOD;
+            wait for CLK_PERIOD;
+            wait for CLK_PERIOD;
+            z_n_re <= z_np1_re;
+            z_n_im <= z_np1_im;
+        end loop;
+        wait;
+    end process test_process;
+end Behavioral;
